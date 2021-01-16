5b628799531076959174f0e305761c9ceed39eec
Zsolt Kardos <zsolt.kardos@hotmail.hu>
2021-01-16 09:25:45 +0100

    first commit

---
+++ ManagerWorker/Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ManagerWorker
+{
+    public class Employee
+    {
+        public int Salary { get; set; }
+
+        #region Solution 1
+        public virtual int GetTotalSalary(int sum)
+        {
+            return 0;
+        }
+        #endregion
+        public int GetTotalSalary2(Employee employee) => employee switch
+        {
+            Manager m => m.GetTotalSalary() + Salary,
+            Worker w => w.Salary,
+            _ => 0,
+        };
+    }
+
+    public class Manager : Employee
+    {
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+
+        #region Solution 1
+        //public override int GetTotalSalary(int sum)
+        //{   
+        //    Employees.ForEach(x => 
+        //    {
+        //        sum += x.GetTotalSalary(sum);                
+        //    });
+        //    return sum += Employees.Sum(x => x.Salary);
+        //}
+        #endregion
+
+        public int GetTotalSalary()
+        {
+            int sum = 0;
+            Employees.ForEach(x =>
+            {
+                sum += x.GetTotalSalary2(x);
+            });
+            return sum;
+        }
+        
+    }
+
+    public class Worker : Employee
+    {
+
+    }
+}
---
+++ ManagerWorker/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ManagerWorker
+{
+    class Program
+    {
+        private static List<Manager> Managers { get; set; } = new List<Manager>();
+        static void Main(string[] args)
+        {
+            SeedData();
+            Managers.ForEach(x =>
+            {
+                Console.WriteLine(x.GetTotalSalary());
+            });
+
+            Console.ReadKey();
+        }
+
+        private static void SeedData()
+        {
+            Manager manager = new Manager();            
+            manager.Employees.Add(new Manager() { Salary = 200  });
+            manager.Employees.Add(new Worker() { Salary = 150  });
+            manager.Employees.Add(SeedData2());
+            manager.Employees.Add(new Worker() { Salary = 100  });
+            Managers.Add(manager);
+        }
+
+        private static Manager SeedData2()
+        {            
+            Manager manager = new Manager();
+            manager.Salary = 250;
+            manager.Employees.Add(new Manager() { Salary = 20 });
+            manager.Employees.Add(new Worker() { Salary = 15 });
+            manager.Employees.Add(new Manager() { Salary = 25 });
+            manager.Employees.Add(new Worker() { Salary = 10 });
+            return manager;
+        }
+    }
+}
